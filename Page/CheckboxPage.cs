dc347a206e3575fb7b0f87827ef4e287a17a66ad
Vera <vdjakova@gmail.com>
2022-04-01 21:34:28 +0300

    Lesson 3

---
+++ Page/CheckboxPage.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace DemoTests.Page
+{
+    public class CheckboxPage : BasePage
+    {
+        private IWebElement firstCheckbox => Driver.FindElement(By.Id("isAgeSelected"));
+        private IWebElement resultElement => Driver.FindElement(By.Id("txtAge"));
+        private IReadOnlyCollection<IWebElement> checkboxes => Driver.FindElements(By.CssSelector(".cb1-element"));
+        private IWebElement button => Driver.FindElement(By.Id("check1"));
+
+        public CheckboxPage(IWebDriver webDriver) : base(webDriver)
+        {
+
+        }
+
+        public void ClickFirstChechbox()
+        {
+            CheckFirstCheckbox(true);
+        }
+
+        public void ClickButton()
+        {
+            button.Click();
+        }
+
+        private void CheckFirstCheckbox(bool shouldBeChecked)
+        {
+            if (shouldBeChecked != firstCheckbox.Selected)
+            {
+                firstCheckbox.Click();
+            }
+        }
+
+        public void CheckAllCheckboxes()
+        {
+            CheckFirstCheckbox(false);
+
+            foreach (IWebElement checkbox in checkboxes)
+            {
+                if (!checkbox.Selected)
+                {
+                    checkbox.Click();
+                }
+
+            }
+        }
+
+        public void VerifyButtonText()
+        {
+            Assert.AreEqual("Uncheck All", button.GetAttribute("value"), "Wrong value");
+        }
+
+        public void VerifyTextResult()
+        {
+            Assert.IsTrue("Success - Check box is checked".Equals(resultElement.Text), $"Text is not the same, actual text is {resultElement.Text}");
+        }
+    }
+}
---
+++ Page/FirstInputPage.cs
@@ -0,0 +1,44 @@
+﻿
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace DemoTests.Page
+{
+    public class FirstInputPage : BasePage
+    {
+        private IWebElement firstInputField => Driver.FindElement(By.Id("sum1"));
+        private IWebElement secondInputField => Driver.FindElement(By.Id("sum2"));
+        private IWebElement calculateButton => Driver.FindElement(By.CssSelector("#gettotal > button"));
+        private IWebElement resultFromPage => Driver.FindElement(By.Id("displayvalue"));
+
+        public FirstInputPage(IWebDriver webdriver) : base (webdriver) { }
+
+        public void InsertFirstInput(string firstParameter)
+        {
+            firstInputField.Clear();
+            firstInputField.SendKeys(firstParameter);
+        }
+
+        public void InsertSecondInput(string secondParameter)
+        {
+            secondInputField.Clear();
+            secondInputField.SendKeys(secondParameter);
+        }
+
+        public void InsertBothInputs(string firstInput, string secondInput)
+        {
+            InsertFirstInput(firstInput);
+            InsertSecondInput(secondInput);
+        }
+
+        public void ClickCalculateButton()
+        {
+            calculateButton.Click();
+        }
+
+        public void VerifyResult(string sum)
+        {
+            Assert.AreEqual(sum, resultFromPage.Text, "Actual result differs from expected");
+        }
+    }
+}
