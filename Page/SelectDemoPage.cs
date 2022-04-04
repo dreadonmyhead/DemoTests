766c306d528b60553b65fd265ad5f102e565c022
Vera <vdjakova@gmail.com>
2022-04-04 21:39:44 +0300

    Lesson 4

---
+++ Page/SelectDemoPage.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+
+namespace DemoTests.Page
+{
+    public class SelectDemoPage : BasePage
+    {
+        private const string address = "http://demo.seleniumeasy.com/basic-select-dropdown-demo.html";
+
+        private const string staticText = "Day selected :- ";
+        private SelectElement dropdownElement => new SelectElement(Driver.FindElement(By.Id("select-demo")));
+        private SelectElement multiDropdownElement => new SelectElement(Driver.FindElement(By.Id("multi-select")));
+        private IWebElement resultElement => Driver.FindElement(By.CssSelector(".selected-value"));
+
+        public SelectDemoPage(IWebDriver webdriver) : base(webdriver) { }
+
+        public void NavigateToPage()
+        {
+            if (Driver.Url != address)
+            {
+                Driver.Url = address;
+            }
+        }
+
+        public void SelectFromDropdownByIndex(int index)
+        {
+            dropdownElement.SelectByIndex(index);
+        }
+
+        public void SelectFromDropdownByVisibleTest(string visibleText)
+        {
+            dropdownElement.SelectByText(visibleText);
+        }
+
+        public void SelectFromDropdownByValue(DayOfWeek value)
+        {
+            dropdownElement.SelectByValue(value.ToString());
+        }
+
+        public void SelectFromMultipleDropDownByText(string text)
+        {
+            multiDropdownElement.DeselectAll();
+            multiDropdownElement.SelectByText(text);
+        }
+
+        public void SelectFromMultipleDropdown(List<string> states)
+        {
+            multiDropdownElement.DeselectAll();
+
+            Actions action = new Actions(Driver);
+
+            foreach (IWebElement option in multiDropdownElement.Options)
+            {
+                action.KeyDown(Keys.Control);
+                if (states.Contains(option.Text))
+                {
+                    if (!option.Selected)
+                    {
+                        option.Click();
+                    }
+                }
+            }
+            action.KeyUp(Keys.Control);
+            action.Build().Perform();
+        }
+
+        public void VerifySelectedOption(string resultText)
+        {
+            IWebElement selectedOption = multiDropdownElement.SelectedOption;
+
+            Assert.AreEqual(resultText, selectedOption.Text, $"Wrong selected option text, actual {selectedOption.Text}");
+        }
+
+        public void VerifySelectedOptions(List<string> states)
+        {
+            foreach (IWebElement option in multiDropdownElement.Options)
+            {
+                if (states.Contains(option.Text))
+                {
+                    Assert.IsTrue(option.Selected);
+                }
+            }
+
+        }
+
+        public void VerifyResultText(DayOfWeek result)
+        {
+            string expectedResultText = staticText + result.ToString();
+
+            WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(5));
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.TextToBePresentInElement(resultElement, expectedResultText));
+
+            Assert.IsTrue(resultElement.Text.Contains(result.ToString()), $"Text is wrong, actual text is {resultElement.Text}");
+        }
+
+    }
+}
---
+++ Test/SelectDemoTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using DemoTests.Page;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace DemoTests.Test
+{
+    public class SelectDemoTest
+    {
+
+        private static IWebDriver driver;
+        private static SelectDemoPage page;
+
+        [OneTimeSetUp]
+        public static void OneTimeSetUp()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+            page = new SelectDemoPage(driver);
+        }
+
+        [OneTimeTearDown]
+        public static void OneTimeTearDown()
+        {
+            driver.Quit();
+        }
+
+        [TestCase(DayOfWeek.Monday, TestName = "Testing Monday")]
+        [TestCase(DayOfWeek.Tuesday, TestName = "Testing Tuesday")]
+        [TestCase(DayOfWeek.Wednesday, TestName = "Testing Wednesday")]
+        [TestCase(DayOfWeek.Friday, TestName = "Testing Friday")]
+        public static void TestSingleDropdown(DayOfWeek dayOfweek)
+        {
+            page.NavigateToPage();
+            page.SelectFromDropdownByValue(dayOfweek);
+            page.VerifyResultText(dayOfweek);
+        }
+
+        [Test]
+        public static void TestMultipleDropdownWithOneOption()
+        {
+            string myTestText = "Ohio";
+            page.NavigateToPage();
+            page.SelectFromMultipleDropDownByText(myTestText);
+            page.VerifySelectedOption(myTestText);
+        }
+
+        [TestCase("California", "New York", TestName = "Test California, New York")]
+        [TestCase("Pennsylvania", "Ohio", "Florida", TestName = "Test Pennsylvania, Ohio, Florida")]
+        [TestCase("Washington", "California", "Texas", "Ohio", TestName = "Test Washington, California, Texas, Ohio")]
+        public static void TestMultipleDropdown(params string[] states)
+        {
+            page.NavigateToPage();
+            page.SelectFromMultipleDropdown(states.ToList());
+            page.VerifySelectedOptions(states.ToList());
+
+        }
+    }
+}
